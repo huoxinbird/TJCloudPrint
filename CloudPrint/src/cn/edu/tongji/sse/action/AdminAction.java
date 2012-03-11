b67f77d26060f0611955c13a9aa3eb3522e93fc5
huoxinbird <magicbird129@gmail.com>
2012-03-11 22:20:30 +0800

    add DI jar, add Client.java, add account.xml

--- CloudPrint/src/cn/edu/tongji/sse/action/AdminAction.java
+++ CloudPrint/src/cn/edu/tongji/sse/action/AdminAction.java
@@ -2,6 +2,8 @@ package cn.edu.tongji.sse.action;
 
 import javax.servlet.http.HttpServletRequest;
 import org.apache.struts2.interceptor.ServletRequestAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class AdminAction implements ServletRequestAware {
 
@@ -23,6 +25,11 @@ public class AdminAction implements ServletRequestAware {
 	public String execute() {
 		String hostName = this.servletRequest.getHeader("Host");
 		System.out.println(hostName);
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("account.xml");
+				
+		//Knight knight = (Knight) context.getBean("knight");
+		
 		return "success";
 	}
 	
---
+++ CloudPrint/src/cn/edu/tongji/sse/admin/Client.java
@@ -0,0 +1,21 @@
+package cn.edu.tongji.sse.admin;
+
+public class Client {
+	private String clientId;
+	private String clientSecret;
+	
+	public String getClientId() {
+		return clientId;
+	}
+	public void setClientId(String clientId) {
+		this.clientId = clientId;
+	}
+	public String getClientSecret() {
+		return clientSecret;
+	}
+	public void setClientSecret(String clientSecret) {
+		this.clientSecret = clientSecret;
+	}
+	
+	
+}
