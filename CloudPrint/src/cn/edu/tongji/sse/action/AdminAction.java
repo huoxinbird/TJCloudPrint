2adee624d70fab6a67ed2d3a3e69abf2d72e26fb
huoxinbird <magicbird129@gmail.com>
2012-03-12 10:46:30 +0800

    check host is localhost

--- CloudPrint/src/cn/edu/tongji/sse/action/AdminAction.java
+++ CloudPrint/src/cn/edu/tongji/sse/action/AdminAction.java
@@ -1,5 +1,7 @@
 package cn.edu.tongji.sse.action;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletRequest;
 import org.apache.struts2.interceptor.ServletRequestAware;
 import org.springframework.context.ApplicationContext;
@@ -32,32 +34,42 @@ public class AdminAction implements ServletRequestAware {
 	public String authorize() {
 		String hostName = this.servletRequest.getHeader("Host");
 		System.out.println(hostName);
+		System.out.println(this.code);
 		
-
+		if (hostName.startsWith("localhost")) {
+			System.out.println("startsWith");
+			//requestAccessToken(this.code);
+		}
+		else {
+			return "error";
+		}
 		
 		
 		return "success";
 	}
 	
-	static void requestAccessToken(String code, String redirectUrl) throws IOException {
-	    ApplicationContext context = new ClassPathXmlApplicationContext("account.xml");
-				
-		Client client = (Client) context.getBean("client");  
-		
+	private void requestAccessToken(String code) throws IOException {
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"account.xml");
+
+		Client client = (Client) context.getBean("client");
+
 		try {
-	        AuthorizationCodeGrant request = new AuthorizationCodeGrant();
-	        request.authorizationServerUrl = "https://accounts.google.com/o/oauth2/token";
-	        request.clientId = client.getClientId();
-	        request.clientSecret = client.getClientSecret();
-	        request.code = code;
-	        request.redirectUri = redirectUrl;
-	        
-	        AccessTokenResponse response = request.execute().parseAs(AccessTokenResponse.class);
-	        System.out.println("Access token: " + response.accessToken);
-	      } catch (HttpResponseException e) {
-	        AccessTokenErrorResponse response = e.response.parseAs(AccessTokenErrorResponse.class);
-	        System.out.println("Error: " + response.error);
-	      }
-	    }
-	
+			AuthorizationCodeGrant request = new AuthorizationCodeGrant();
+			request.authorizationServerUrl = "https://accounts.google.com/o/oauth2/token";
+			request.clientId = client.getClientId();
+			request.clientSecret = client.getClientSecret();
+			request.code = code;
+			request.redirectUri = "http://localhost";
+
+			AccessTokenResponse response = request.execute();
+			System.out.println("Access token: " + response.accessToken);
+			
+		} catch (HttpResponseException e) {
+			AccessTokenErrorResponse response = e.getResponse()
+					.parseAs(AccessTokenErrorResponse.class);
+			System.out.println("Error: " + response.error);
+		}
+	}
+
 }
